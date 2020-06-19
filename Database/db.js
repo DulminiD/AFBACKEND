e561f92daa332634effa707da6ea81511c44d712
DulminiD <53858570+DulminiD@users.noreply.github.com>
2020-06-19 10:50:00 +0530

    up db.js

--- Database/db.js
+++ Database/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     db:'mongodb+srv://Lakshan:Lakshan.123@cluster0-izwar.gcp.mongodb.net/react?retryWrites=true&w=majority',
     secretOrKey: "secret"
-    //mongo account 
+    //mongo account 11
 }
